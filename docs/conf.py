2a9beeb1c041dbb87b0fb2d1f5af1742f42ef88c
burgholzer <burgholzer@me.com>
2025-03-25 00:49:27 +0100

    📈 update overview

--- docs/conf.py
+++ docs/conf.py
@@ -68,6 +68,9 @@ intersphinx_mapping = {
     "qudits": ("https://mqt.readthedocs.io/projects/qudits/en/latest/", None),
     "qubomaker": ("https://mqt.readthedocs.io/projects/qubomaker/en/latest/", None),
     "syrec": ("https://mqt.readthedocs.io/projects/syrec/en/latest/", None),
+    "qao": ("https://mqt.readthedocs.io/projects/qao/en/latest/", None),
+    "yaqs": ("https://mqt.readthedocs.io/projects/yaqs/en/latest/", None),
+    "debugger": ("https://mqt.readthedocs.io/projects/debugger/en/latest/", None),
 }
 intersphinx_disabled_reftypes = ["*"]
 
